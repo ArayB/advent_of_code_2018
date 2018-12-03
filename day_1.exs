05dc460e759550229d73d10ecc3f8c215a31c0f4
Ant Barnes <anthony.barnes@thefloow.com>
2018-12-03 13:31:34 +0000

    Day one solved

---
+++ day_1.exs
@@ -0,0 +1,68 @@
+defmodule Exercise1 do
+  def frequency(input) do
+    Enum.reduce(
+      Enum.map(input, fn s ->
+        {a, _} = Integer.parse(s)
+        a
+      end),
+      fn x, acc -> x + acc end
+    )
+  end
+end
+
+defmodule Exercise2 do
+  def duplicate_frequency(input) do
+    find_freq(input, input, [0], 0)
+  end
+
+  defp find_freq([], original_input, frequencies, last_frequency) do
+    find_freq(original_input, original_input, frequencies, last_frequency)
+  end
+
+  defp find_freq([head | tail], original_input, frequencies, last_frequency) do
+    {change, _} = Integer.parse(head)
+    new_freq = last_frequency + change
+
+    if Enum.member?(frequencies, new_freq) do
+      new_freq
+    else
+      find_freq(tail, original_input, frequencies ++ [new_freq], new_freq)
+    end
+  end
+end
+
+ExUnit.start()
+
+defmodule Exercise1Test do
+  use ExUnit.Case
+
+  import Exercise1
+
+  test "simple" do
+    assert frequency(["+1", "+1", "+1"]) == 3
+    assert frequency(["+1", "+1", "-2"]) == 0
+    assert frequency(["-1", "-2", "-3"]) == -6
+  end
+
+  test "with input" do
+    assert File.read!("input_1.txt") |> String.split() |> frequency == 474
+  end
+end
+
+defmodule Exercise2Test do
+  use ExUnit.Case
+
+  import Exercise2
+
+  test "simple" do
+    assert duplicate_frequency(["-1", "+1"]) == 0
+    assert duplicate_frequency(["+3", "+3", "+4", "-2", "-4"]) == 10
+    assert duplicate_frequency(["-6", "+3", "+8", "+5", "-6"]) == 5
+    assert duplicate_frequency(["+7", "+7", "-2", "-7", "-4"]) == 14
+  end
+
+  test "with input" do
+    # This takes a while to run!
+    # assert File.read!("input_1.txt") |> String.split() |> duplicate_frequency == 137_041
+  end
+end
