4afe04836ce68cb9a0e041a84f5cc88af6fb6870
Ant Barnes <anthony.barnes@thefloow.com>
2018-12-05 09:21:38 +0000

    Tweak day 1

--- day_1.exs
+++ day_1.exs
@@ -7,7 +7,21 @@ end
 defmodule Exercise2 do
   def duplicate_frequency(input) do
     find_freq(input, input, [0], 0)
+    # find_freq(input, [0], 0)
   end
+
+  # Tried without passing the original list around and just adding tested transforms to the end of the list
+  # Was much faster than the original solution but the speed gain all came from prepending discovered frequencies instead of concatenating
+  # This method is a little slower than passing original list around, because it does a lot of list concatenation
+  # defp find_freq([head | tail], frequencies, last_frequency) do
+  #   new_freq = last_frequency + String.to_integer(head)
+  #
+  #   if Enum.member?(frequencies, new_freq) do
+  #     new_freq
+  #   else
+  #     find_freq(tail ++ [head], [new_freq | frequencies], new_freq)
+  #   end
+  # end
 
   defp find_freq([], original_input, frequencies, last_frequency) do
     find_freq(original_input, original_input, frequencies, last_frequency)
@@ -20,12 +34,17 @@ defmodule Exercise2 do
     if Enum.member?(frequencies, new_freq) do
       new_freq
     else
-      find_freq(tail, original_input, frequencies ++ [new_freq], new_freq)
+      # changing this line from concatenating list to prepending dropped execution time from 60s to 16s
+      # It doesn't matter what order the discovered frequencies are in so prepending is fine
+      # find_freq(tail, original_input, frequencies ++ [new_freq], new_freq)
+
+      find_freq(tail, original_input, [new_freq | frequencies], new_freq)
     end
   end
 end
 
 ExUnit.start()
+# ExUnit.start(timeout: 3_600_000)
 
 defmodule Exercise1Test do
   use ExUnit.Case
@@ -56,7 +75,7 @@ defmodule Exercise2Test do
   end
 
   test "with input" do
-    # This takes a while to run!
-    # assert File.read!("input_1.txt") |> String.split() |> duplicate_frequency == 137_041
+    # This takes about 15s to run
+    assert File.read!("input_1.txt") |> String.split() |> duplicate_frequency == 137_041
   end
 end
